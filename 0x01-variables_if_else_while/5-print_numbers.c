7aa375793f232ceef4193313044a6e41b3593ba9
tonycharles377 <tonycharles377@gmail.com>
2022-12-11 04:03:19 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print all single digit base 10 numbers
+ *
+ * 0 - 9
+ *
+ * followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
