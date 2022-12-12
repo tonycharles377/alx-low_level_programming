fb9892e4ec7b311388ee054c6559bd2bc34de70e
tonycharles377 <tonycharles377@gmail.com>
2022-12-12 08:11:14 -0800

    correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,17 +9,15 @@
 
 int main(void)
 {
-	printf("Size of a char is %zd byte(s)\n", sizeof(char));
+	printf("Size of a char: %zd byte(s)\n", sizeof(char));
 
-	printf("Size of an int is %zd byte(s)\n", sizeof(int));
+	printf("Size of an int: %zd byte(s)\n", sizeof(int));
 
-	printf("Size of an double is %zd byte(s)\n", sizeof(double));
+	printf("Size of a long int: %zd byte(s)\n", sizeof(long int));
 
-	printf("Size of a long int is %zd byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zd byte(s)\n", sizeof(long long int));
 
-	printf("Size of a long long int is %zd byte(s)\n", sizeof(long long int));
-
-	printf("Size of a float is %zd byte(s)\n", sizeof(float));
+	printf("Size of a float: %zd byte(s)\n", sizeof(float));
 
 	return (0);
 }
