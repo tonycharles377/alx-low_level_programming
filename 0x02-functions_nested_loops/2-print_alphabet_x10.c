eca3c1b8e9fc3f065c63594cb6025b454051eb54
tonycharles377 <tonycharles377@gmail.com>
2022-12-14 01:26:43 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * print an alphabet x10
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i = 0;
+
+	while (int <= 9)
+	{
+		_putchar(c);
+		i++;
+	}
+	_putchar('\n');
+}
