a4a2c15e27cc9b59f7cf8f9eaa1ee439c3e602a9
tonycharles377 <tonycharles377@gmail.com>
2023-01-09 11:48:27 -0800

    libmy.a

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_isalpha - checks if a character is a letter
+ *both upper and lower case
+ *@c: takes input from other functions
+ *Return: 1 if c is true else 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a'  && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+
+	return (0);
+}
