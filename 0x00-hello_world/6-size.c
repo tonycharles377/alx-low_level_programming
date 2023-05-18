af709efe15629c8b2fa280e3873476eadb86f6c9
charles omondi <tonycharles377@gmail.com>
2023-05-18 13:16:51 -0700

    Fix betty stile and typos

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - prints size of various types 
+ * main - prints size of various types
  *
  * Return: 0
  */
@@ -9,9 +9,9 @@ int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of an long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of an long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of an float: %d byte(s)\n", sizeof(float));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
 }
