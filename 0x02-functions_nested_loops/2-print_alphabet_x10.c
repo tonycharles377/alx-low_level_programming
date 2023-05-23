63309f3e134fb1740563c62625ad722429532e83
charles omondi <tonycharles377@gmail.com>
2023-05-23 10:47:16 -0700

    Print 10 times the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 11; i++)
+	{
+		for (j = 97; j < 123; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
