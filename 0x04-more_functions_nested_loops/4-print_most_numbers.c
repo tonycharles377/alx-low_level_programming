cdafa67d1a31324d933c44cde6cc77b5806cbefa
charles omondi <tonycharles377@gmail.com>
2023-05-25 11:48:43 -0700

    Print decimal 0 to 9 excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_most_numbers  -  prints the numbers, from 0 to 9
+ * Do not print 2 and 4
+ * use _putchar twice
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
