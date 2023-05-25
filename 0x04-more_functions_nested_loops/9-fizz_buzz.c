13c5777656170d2a9a182fca7ac321fc9169eae0
charles omondi <tonycharles377@gmail.com>
2023-05-25 13:15:01 -0700

    Fizz-Buzz technical quiz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ * main - prints the numbers from 1 to 100
+ * for multiples of three print Fizz
+ * for the multiples of five print Buzz
+ * For numbers which are multiples of both three and five print FizzBuzz
+ * Each number or word should be separated by a space
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
