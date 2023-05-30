0acd8d5f9f868c3330e2e0dfd792d8c6b8d325e8
charles omondi <tonycharles377@gmail.com>
2023-05-30 09:09:52 -0700

    Hot fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,16 +10,9 @@ void puts2(char *str)
 	int size = sizeof(str);
 	int i;
 
-	for (i = 0; i <= size; i++)
+	for (i = 0; i <= size; i += 2)
 	{
-		if (i % 2 != 0)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(str[i]);
-		}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
-}	
+}
