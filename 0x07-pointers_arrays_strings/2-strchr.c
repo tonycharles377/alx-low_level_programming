339e03fadad3ad057c532f9e21f09cfac3c5377b
charles omondi <tonycharles377@gmail.com>
2023-06-05 12:07:44 -0700

    strchr function

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include<string.h>
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: char to be located
+ * Return: pointer to the matched char or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return ("Null");
+}
