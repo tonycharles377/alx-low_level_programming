275bea4bb18f98170768c827d2c589a86fb1c84e
charles omondi <tonycharles377@gmail.com>
2023-06-06 04:51:26 -0700

    Hot fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,18 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int a;
+
+	while (1)
 	{
-		if (*s == c)
+		a = *s++;
+		if (a == c)
 		{
-			return (s);
+			return (s - 1);
 		}
-		s++;
+		if (a == 0)
+		{
+			return ('\0');
+		}
 	}
-	return ('\0');
 }
