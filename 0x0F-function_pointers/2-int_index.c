463cd77647935c88092c1565ecf59fe2995ad73b
charles omondi <tonycharles377@gmail.com>
2023-06-21 13:24:09 -0700

    Betty style correction

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,11 @@
 #include<stdlib.h>
+/**
+ * int_index - searches for an integer
+ * @array: array to search from
+ * @size: size of array
+ * @cmp: function
+ * Return: index of int
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
@@ -18,7 +25,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp (array[i]))
+		if (cmp(array[i]))
 		{
 			return (i);
 		}
