a2dc9b0cf988fabea8b207d3b9a82d5623bfbc83
charles omondi <tonycharles377@gmail.com>
2023-06-21 13:20:14 -0700

    Search function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include<stdlib.h>
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	if (cmp == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp (array[i]))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
