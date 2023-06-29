4c4ced839e6276705bd625f81bceb72d8e157b89
charles omondi <tonycharles377@gmail.com>
2023-06-29 10:50:26 -0700

    Handle NULL s1 || s2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,9 +19,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	ptr = malloc((strlen(s1) + n + 1) * sizeof(char));
 
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 	{
