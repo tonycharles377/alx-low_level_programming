6781e8e3024f62cba01fd064aeec0e3d5eff1439
charles omondi <tonycharles377@gmail.com>
2023-07-01 16:37:08 -0700

    assembly code

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .data
+    format db "Hello, Holberton\n", 0
+
+section .text
+    global main
+
+extern printf
+
+main:
+    sub rsp, 8               ; Adjust stack alignment if needed
+    mov edi, format          ; Pass the format string as the first argument
+    xor eax, eax             ; Clear the upper 32 bits of rax
+    call printf              ; Call the printf function
+    add rsp, 8               ; Restore stack alignment if adjusted
+    xor eax, eax             ; Set the exit status to 0
+    ret                      ; Return from the main function
+
