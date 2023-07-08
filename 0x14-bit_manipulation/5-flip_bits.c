e4f6247a45d135c18687fd858da2a0d45aa1a228
charles omondi <tonycharles377@gmail.com>
2023-07-08 14:04:13 -0700

    Hot fix

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,7 +9,8 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int result, count = 0;
+	unsigned long int result;
+	unsigned int count = 0;
 
 	result = n ^ m;
 	while (result > 0)
