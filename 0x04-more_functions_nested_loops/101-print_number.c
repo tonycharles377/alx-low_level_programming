0e3aa239e0287756e879ab1f5cf885a92fc58c0d
tonycharles377 <tonycharles377@gmail.com>
2023-08-30 07:44:49 -0700

    Code fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-void print_number(int n);
 
 /**
  * print_number - prints an integer
@@ -7,58 +6,13 @@ void print_number(int n);
  */
 void print_number(int n)
 {
-	if (n > 0)
-	{
-		if (n <= 9)
-		{
-			_putchar('0' + n);
-		}
-		else if (n <= 99)
-		{
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-		}
-		else if (n <= 999)
-		{
-			_putchar('0' + n / 100);
-			_putchar('0' + (n / 10) % 10);
-			_putchar('0' + n % 10);
-		}
-		else if (n <= 9999)
-		{
-			_putchar('0' + n / 1000);
-			_putchar('0' + (n / 100) % 10);
-			_putchar('0' + (n / 10) % 10);
-			_putchar('0' + n % 10);
-		}
-	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
-		if (n <= 9)
-		{
-			_putchar('0' + n);
-		}
-		else if (n <= 99)
-		{
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-		}
-		else if (n <= 999)
-		{
-			_putchar('0' + n / 100);
-			_putchar('0' + (n / 10) % 10);
-			_putchar('0' + n % 10);
-		}
-		else if (n <= 9999)
-		{
-			_putchar('0' + n / 1000);
-			_putchar('0' + (n / 100) % 10);
-			_putchar('0' + (n / 10) % 10);
-			_putchar('0' + n % 10);
-		}
 	}
-	else
-		_putchar('0' + n);
+	if (n / 10 != 0)
+		print_number(n / 10);
+
+	_putchar('0' + (n % 10));
 }
